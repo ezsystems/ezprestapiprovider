1aaa5879472b270fddbb5733002c68cc079f1597
Lukasz Serwatka <ls@ez.no>
2010-12-16 13:34:58 +0100

    Implemented ezp rest provider Implemented created provider interface
    Pair programmed by:
    Łukasz Serwatka <ls@ez.no>
    Ole Marius Smestad <oms@ez.no>

---
+++ classes/rest_provider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * File containing the ezpRestApiProvider class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ */
+
+class ezpRestApiProvider implements ezpRestProviderInterface
+{
+    /**
+     * Returns registered versioned routs for provider
+     *
+     * @return array
+     */
+    public function getRouts()
+    {
+        return array( new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/node/:nodeId/listAtom', 'ezpRestAtomController', 'collection' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/node/:nodeId/list', 'ezpRestContentController', 'list' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/node/:nodeId', 'ezpRestContentController', 'viewContent' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/node/:nodeId/fields', 'ezpRestContentController', 'viewFields' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/node/:nodeId/field/:fieldIdentifier', 'ezpRestContentController', 'viewField' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/object/:objectId', 'ezpRestContentController', 'viewContent' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/object/:objectId/fields', 'ezpRestContentController', 'viewFields' ), 1 ),
+                      new ezpRestVersionedRoute( new ezcMvcRailsRoute( '/content/object/:objectId/field/:fieldIdentifier', 'ezpRestContentController', 'viewField' ), 1 ) );
+    }
+
+    /**
+     * Returns associated with provider view controller
+     *
+     * @return ezpRestViewController
+     */
+    public function getViewController() {
+        // TODO: Implement getViewController() method.
+    }
+}
---
+++ settings/rest.ini.append.php
@@ -0,0 +1,6 @@
+<?php /*
+
+[ApiProvider]
+ProviderClass[ezp]=ezpRestApiProvider
+
+*/ ?>
