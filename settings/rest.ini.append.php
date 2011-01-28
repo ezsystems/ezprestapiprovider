a159ef855be609946374d477c57f6a692345a38e
lolautruche <lolautruche@gmail.com>
2011-01-28 16:45:35 +0100

    REST Application cache implementation (restore/store features only), cluster aware

--- settings/rest.ini.append.php
+++ settings/rest.ini.append.php
@@ -3,4 +3,13 @@
 [ApiProvider]
 ProviderClass[ezp]=ezpRestApiProvider
 
+# Default cache settings for ezpRestAtomController REST controller
+[ezpRestContentController_CacheSettings]
+ApplicationCache=enabled
+CacheTTL=1200
+
+[ezpRestContentController_viewContent_CacheSettings]
+ApplicationCache=enabled
+CacheTTL=3600
+
 */ ?>
