b10d016cb3109a2b71f86fb6e0d3d80d984a9f09
lolautruche <lolautruche@gmail.com>
2011-02-04 11:44:32 +0100

    Removed specific controller/action settings for content services. Added INI meta info and charset

--- settings/rest.ini.append.php
+++ settings/rest.ini.append.php
@@ -1,15 +1,6 @@
-<?php /*
+<?php /* #?ini charset="utf-8"?
 
 [ApiProvider]
 ProviderClass[ezp]=ezpRestApiProvider
-
-# Default cache settings for ezpRestAtomController REST controller
-[ezpRestContentController_CacheSettings]
-ApplicationCache=enabled
-CacheTTL=1200
-
-[ezpRestContentController_viewContent_CacheSettings]
-ApplicationCache=enabled
-CacheTTL=30
 
 */ ?>
